3cbcc10ad11597d5a80abaf884decbeec21c2b53
sirbaconsalot <donavynhaley@gmail.com>
2021-05-29 18:17:26 -0700

    Created chessboard with chesboardjs. Logic implemented with chessjs. User can play against bot that make a random valid move. Moves for player and bot tracked in a history component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Chessboard from 'chessboardjsx';
+import Chess, { ChessInstance } from "chess.js";
+import MoveHistory from './components/MoveHistory';
 
 function App() {
+  const [chess] = useState(new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+  const [fen, setFen] = useState(chess.fen());
+  const [movesHistory, setMovesHistory] = useState([]);
+  const [selectedBot, setSelectedBot] = useState();
+
+  // add move to history
+  const updateHistory = (newMove) => {
+    const currentMovesHistory = movesHistory;
+    const pieceType = chess.get(newMove.to).type.toUpperCase();
+    let move;
+    // do not display pawn
+    if (pieceType === "P") {
+      move = `${newMove.to}`
+    }
+    else {
+      move = `${pieceType}${newMove.to}`
+    }
+    currentMovesHistory.push(move);
+    setMovesHistory(currentMovesHistory);
+  }
+
+  const handlePlayerMove = (playerMove) => {
+    // Checks if playermove is valid
+    if (chess.move(playerMove)) {
+      // add move to history
+      updateHistory(playerMove);
+
+      // computer response random
+      setTimeout(() => {
+        // Get list of valid moves
+        const moves = chess.moves();
+
+        // If valid move is avaliable selects random and updates chessboard
+        if (moves.length > 0) {
+          const computerMove = moves[Math.floor(Math.random() * moves.length)];
+          chess.move(computerMove);
+
+          // add move to history
+          const currentMovesHistory = movesHistory;
+          currentMovesHistory.push(computerMove);
+          // update chessboard
+          setFen(chess.fen());
+        }
+      }, 300)
+
+      // update chessboard
+      setFen(chess.fen());
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Chess AI</h1>
+      <div className="chess-container">
+        <Chessboard
+          width={400}
+          position={fen}
+          onDrop={(move) => handlePlayerMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })}
+
+        />
+      </div>
+      <div className="utility-container">
+        <MoveHistory movesHistory={movesHistory} chess={chess} />
+
+        <div className="data-visualization">
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/MoveHistory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MoveHistory = props => {
+    const { movesHistory } = props;
+    let chessPieceMap = new Map();
+
+    chessPieceMap.set("p", "Pawn");
+    chessPieceMap.set("r", "Rook");
+    chessPieceMap.set("n", "Knight");
+    chessPieceMap.set("b", "Bishop");
+    chessPieceMap.set("q", "Queen");
+    chessPieceMap.set("k", "King");
+    console.log(movesHistory);
+
+    const listMoveHistory = movesHistory.map((move) => {
+        return <div className={`node`}>{}{move}</div>
+    })
+
+    return (
+        <div className="move-history">
+            <h2>test</h2>
+            {listMoveHistory}
+        </div>
+    )
+}
+
+
+export default MoveHistory;
