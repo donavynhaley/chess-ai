2be2c80674b03b82d71589d95f6892643abddf03
sirbaconsalot <donavynhaley@gmail.com>
2021-06-14 18:54:57 -0700

    skeleton of MiniMax Algorithim complete. Updates computer position based on calculations of board state. Needs fine tuning on depth of 2

--- src/components/Bots/MiniMax.js
+++ src/components/Bots/MiniMax.js
@@ -23,32 +23,66 @@ pieceValues['r'] = 1276;
 pieceValues['q'] = 2538;
 pieceValues['k'] = 9999;
 
+const miniMaxRecursive = (chessCopy, depth, allEval, isMax) => {
+    if (depth === 0) {
+        return getBoardEvaluation(chessCopy.board());
+    }
+    const moves = chessCopy.moves();
+    let bestMove = moves[Math.floor(Math.random() * moves.length)];
+    if (isMax) {
+        let maxEval = -Infinity;
+        for (let i = 0; i < moves.length; i++) {
+            chessCopy.move(moves[i])
+            allEval.push({ score: getBoardEvaluation(chessCopy.board()), move: moves[i], depth: depth })
+            let currentEval = miniMaxRecursive(chessCopy, depth - 1, allEval, false);
+            chessCopy.undo();
+            console.log(maxEval + " max " + currentEval)
+            if (currentEval > maxEval) {
+                maxEval = currentEval;
+                bestMove = moves[i];
+            }
+        }
+        return bestMove;
+
+    }
+    else {
+        let minEval = Infinity;
+        for (let i = 0; i < moves.length; i++) {
+            chessCopy.move(moves[i])
+            allEval.push({ score: getBoardEvaluation(chessCopy.board()), move: moves[i], depth: depth })
+            let currentEval = miniMaxRecursive(chessCopy, depth - 1, allEval, true);
+            chessCopy.undo()
+            console.log(minEval + " min " + currentEval)
+
+            if (currentEval < minEval) {
+                minEval = currentEval;
+                bestMove = moves[i];
+            }
+        }
+        return bestMove
+    }
+
+}
 const MiniMax = (chess, updateComputerHistory, setRandomBotAvaliableMoves, setFen) => {
+    const originalFen = chess.fen();
     const allEval = []
     const chessCopy = new Chess(chess.fen());
-
+    allEval.push({ score: getBoardEvaluation(chess.board), move: 'original', depth: 2 })
     // computer response random
     setTimeout(() => {
+        const bestMove = miniMaxRecursive(chessCopy, 2, allEval, originalFen, true)
+        console.log(bestMove)
+        console.log(allEval)
 
-        // Get list of valid moves
-        const moves = chessCopy.moves();
-        console.log(moves)
-        getBoardEvaluation(chessCopy.board())
-        // update state for data vizualization
-        // setRandomBotAvaliableMoves(moves);
+        // update position
+        chess.move(bestMove)
 
-        // If valid move is avaliable selects random and updates chessboard
-        if (moves.length > 0) {
-            const computerMove = moves[Math.floor(Math.random() * moves.length)];
-            chess.move(computerMove);
+        // add move to history
+        updateComputerHistory(bestMove);
 
-            // add move to history
-            updateComputerHistory(computerMove);
-
-            // update chessboard
-            setFen(chess.fen());
-        }
-    }, 300)
+        // update chessboard
+        setFen(chess.fen());
+    }, 1000)
 }
 const getBoardEvaluation = (board) => {
     let count = 0;
@@ -66,7 +100,7 @@ const getBoardEvaluation = (board) => {
             }
         }
     }
-    console.log(count)
+    return count
 }
 
 export default MiniMax
